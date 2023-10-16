65e5dcbcc2313345bc2f09c7eabac0703901edda
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-10-16 23:49:27 +0200

    Added Shader Class; figuring out why the triangle isn't showing up

---
+++ src/Shader.h
@@ -0,0 +1,26 @@
+#ifndef OPENGLGP_SHADER_H
+#define OPENGLGP_SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+class Shader {
+public:
+    // ID program object
+    unsigned int ID;
+
+    // Constructor which reads shaders from the disk and creates it
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    void use();
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
+
+#endif //OPENGLGP_SHADER_H
--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,10 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include "Shader.h"
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 position;\n"
-                                 "void main() {\n"
-                                 "      gl_Position = vec4(position, 1.0);"
-                                 "}\0";
-
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 color;\n"
-                                   "uniform vec4 ourColor;"
-                                   "void main() {\n"
-                                   "    color = ourColor;\n"
-                                   "}\0";
 
 int main()
 {
@@ -26,7 +14,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // GLFW 3.3
+#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required for macOS
+#endif
 
     // Create window variable
     GLFWwindow* window = glfwCreateWindow(800, 600, "Fun with shaders!", nullptr, nullptr);
@@ -50,57 +40,14 @@ int main()
     // Change the draw range when changing the window size
     glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-    // Build and compile shader program
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1,&vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-
-    GLint success;
-    GLchar infoLog[512];
-
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    } else {
-        std::cout << "VertexShader\t-\tSuccess!" << std::endl;
-    }
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    } else {
-        std::cout << "FragmentShader\t-\tSuccess!" << std::endl;
-    }
-
-    // Link shader programs
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cout << "ERROR::PROGRAM::OBJECT::COMPILATION_ERROR\n" << infoLog << std::endl;
-    } else {
-        std::cout << "ShaderProgram\t-\tSuccess!" << std::endl;
-    }
-
-    // Delete shader objects after linkage
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader shader("res/shaders/basic.vert", "res/shaders/basic.frag");
 
     // Set up vertex data and buffers
     GLfloat vertices[] = {
-    -0.5f,  -0.5f, 0.0f,     // Bottom left
-     0.5f, -0.5f, 0.0f,      // Bottom right
-     0.0f, 0.5f, 0.0f,       // Top
+            // Position                     // Colors
+        -0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, // Bottom left
+        0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,// Bottom right
+        0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f// Top
     };
 
     GLuint VBO, VAO;
@@ -112,10 +59,14 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) nullptr);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) nullptr);
     glEnableVertexAttribArray(0);
 
-    glBindVertexArray(VAO);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
     // Game loop - update buffers to create shapes and animations
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -123,14 +74,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
-
-        double timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue)) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-
+        shader.use();
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
@@ -139,7 +84,6 @@ int main()
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
 
     // Terminate all resources used to create window
     glfwTerminate();
